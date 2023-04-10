c952e97962a117002468a3092218e7a4a072af8a
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2023-04-10 15:50:01 +0300

    add mobile view

---
+++ catalog.js
@@ -0,0 +1,32 @@
+const partsData = {
+    'frame': [
+        ['Carbon Fiber', 40000],
+        ['Titanium', 30000],
+        ['Aluminium', 20000]
+        ],
+    'seat': [
+        ['Polished Leather', 1500],
+        ['Faux Leather', 1400],
+        ['PVC', 1300],
+        ['Reindeer Pelt', 1200],
+        ],
+    'engine': [
+        ['Thunder (150 HP)', 2000],
+        ['Flash (50 HP)', 1400],
+        ['Spark (5 HP)', 1300]
+        ],
+    'battery': [
+        ['Li-ion (400 km*)', 10000],
+        ['Li-ion (200 km*)', 6000],
+        ['Lead Acid (80 km*)', 7000],
+        ['AAA (2 km*)', 1000],
+        ],
+    'tires': [
+        ['Urban', 400],
+        ['All-road', 800],
+        ['Off-road', 1600]
+        ],
+    'color': [
+        ['HSL', 400],
+        ],
+    }
---
+++ funcs.js
@@ -0,0 +1,69 @@
+let hue = 0;
+let saturation = 1;
+let lightness = 1;
+
+const formatPrice = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+const changeBikeFilter = () => {
+    document.getElementById('colorhue').setAttribute('style', `filter: hue-rotate(${hue}deg) saturate(${saturation}) brightness(${lightness});`)
+    document.getElementById('color_thumb').setAttribute('style', `filter: hue-rotate(${hue}deg) saturate(${saturation}) brightness(${lightness});`)
+}
+const updateBikeColors = (param, value) => {
+    if(param == 'h'){
+            hue = value;
+    }
+    else if(param == 's'){
+            saturation = value/100;
+    }
+    else if(param == 'l'){
+            lightness = value/100;
+    }
+    
+    changeBikeFilter()
+}
+
+const changeThumbnail = (partName, itemNumber) => {
+    const elem = `${partName}_option`
+    const thumbnailElem = document.querySelector(`#${elem}>img`)
+    const source = thumbnailElem.getAttribute('src')
+    thumbnailElem.setAttribute('src', `img/thumbs/${partName}${itemNumber}.png`)
+}
+const updatePrice = () => {
+    let totalSum = 0
+    document.querySelectorAll('#parts i').forEach(element => {
+        totalSum += parseInt(element.innerHTML.replace(' ',''))
+    });
+
+    let price = formatPrice(totalSum)
+    document.querySelector("#totalAmount span").innerHTML = `${price} €`;
+}
+const updateMotorcycle = (partName, itemNumber) => {
+    const imgElem = document.getElementById(partName)
+    imgElem.setAttribute('src', `img/preview/${partName}${itemNumber}.png`)  
+}
+const updatePartsList = (item, itemNumber) => {
+    const elem = document.getElementById(`${item}_txt`)
+    elem.querySelector('p').innerHTML = partsData[item][itemNumber-1][0]
+    elem.querySelector('i').innerHTML = formatPrice(partsData[item][itemNumber-1][1]) + ' €'
+}
+const updateView = (item) => {
+    const partName = item.slice(0, -1);
+    const itemNumber = parseInt(item.slice(-1));
+
+    changeThumbnail(partName, itemNumber)   
+    updateMotorcycle(partName, itemNumber)
+    updatePartsList(partName, itemNumber)    
+    updatePrice()
+}
+const darkMode = () => {
+    var checkBox = document.getElementById("darkmode");
+    var editorElem = document.getElementById("editor");
+    if (checkBox.checked == true){
+        editorElem.className = 'dark'
+    } else {
+            editorElem.className = 'light'
+    }
+}
+
